3b5fb03637c48c39a7cee75626f121fea85afdb5
deepakm22 <deepakmanikandan67@gmail.com>
2024-09-06 17:37:54 +0530

    uploading a file

--- month-project-2/todo-list/backend/middlewares/auth.js
+++ month-project-2/todo-list/backend/middlewares/auth.js
@@ -1,26 +1,22 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
-
-
-const token = req.headers['authorization'];
-
-if (!token) {
-    return res.status(401).json({ error: 'Token not provided!' });
-}
-
-
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    console.log(decoded);
-
-    if (!decoded) {
-    return res.status(403).json({ error: 'invalid token' });
+    const authHeader = req.headers['authorization'];
+    
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Token not provided!' });
     }
 
-    req.user = decoded;
-    next();
+    const token = authHeader.split(' ')[1]; // Extract token from 'Bearer <token>' format
 
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: 'Invalid or expired token' });
+    }
 };
 
 module.exports = authenticateToken;
--- month-project-2/todo-list/backend/models/Task.js
+++ month-project-2/todo-list/backend/models/Task.js
@@ -1,16 +1,17 @@
+// models/Task.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const User = require('./User');
 
 const Task = sequelize.define('Task', {
     id: { 
-        type: DataTypes.INTEGER,
+        type: DataTypes.INTEGER, 
         primaryKey: true, 
         autoIncrement: true 
     },
     title: { 
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING, 
+        allowNull: false 
     },
     description: { 
         type: DataTypes.STRING 
